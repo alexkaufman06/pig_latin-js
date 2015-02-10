89e31dc00368682f8e4bc12fe94f695e7c3732e9
J. TURNER <turner.j6@gmail.com>
2015-02-09 16:44:54 -0800

    Add js files.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 var pigLatin = function(word) {
-  var vowels = ["a", "e", "i", "o", "u"]
+  var vowels = ["a", "e", "i", "o", "u"];
+  var letters = word.split("");
   if (vowels.indexOf(word[0]) !== -1) {
-    return word.concat("ay");
+    return word + "ay";
+  } else {
+    return (word.slice(1) + word.slice(0,1) + "ay");
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,18 @@ describe('pigLatin', function() {
   it('adds ay to the end of words that start with a vowel', function() {
     expect(pigLatin("apple")).to.equal("appleay")
   });
+
+  it('moves consecutive consonants to the end of word and adds ay to it', function() {
+    expect(pigLatin("toy")).to.equal("oytay")
+  });
 });
+
+// var wordsVowels = []
+// letters.each() |letter|
+//   vowels.indexOf(letter) !== -1
+//     wordsVowels.push(letter)
+//   end
+// end
+// word.slice(word.indexOf(wordsVowels[0])).concat(word[0]).concat("ay")
+
+// return word.slice(1) + word.slice(0,1) + "ay"
